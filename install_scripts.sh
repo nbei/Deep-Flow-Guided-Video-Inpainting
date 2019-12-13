e95cc625969a2dd561ecc3967fc1cd2e9c8d6ff9
nbei <631557085@qq.com>
2019-12-13 12:05:46 +0800

    flownet

--- install_scripts.sh
+++ install_scripts.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 cd ./models/FlowNet2_Models/resample2d_package/
-./make.sh
+rm -rf *_cuda.egg-info build dist __pycache__
+python3 setup.py install --develop
+
 cd ../correlation_package
-./make.sh
+rm -rf *_cuda.egg-info build dist __pycache__
+python3 setup.py install --develop
+
 cd ../channelnorm_package
-./make.sh
+rm -rf *_cuda.egg-info build dist __pycache__
+python3 setup.py install --develop
 cd ../..
