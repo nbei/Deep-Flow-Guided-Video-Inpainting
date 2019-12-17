34b1ffff95e77d08ca40559cced237ce77fe5200
nbei <nbeiray@gmail.com>
2019-12-17 15:39:22 +0800

    fix

--- install_scripts.sh
+++ install_scripts.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 cd ./models/FlowNet2_Models/resample2d_package/
 rm -rf *_cuda.egg-info build dist __pycache__
-python3 setup.py install --develop
+python3 setup.py develop
 
 cd ../correlation_package
 rm -rf *_cuda.egg-info build dist __pycache__
-python3 setup.py install --develop
+python3 setup.py develop
 
 cd ../channelnorm_package
 rm -rf *_cuda.egg-info build dist __pycache__
-python3 setup.py install --develop
+python3 setup.py develop
 cd ../..
